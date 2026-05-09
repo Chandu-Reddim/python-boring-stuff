6fee02142aa6b27072fddfbc5a553c8aee25a16d
chandu reddim <chandu.reddim@gmail.com>
2026-05-09 15:59:53 +0530

    more about Fstring

--- FstringExp.py
+++ FstringExp.py
@@ -5,3 +5,14 @@ def formatVsFstring():
     print(f"hello {name}")
 
 dis.dis(formatVsFstring)
+
+'''F string are better than format string as it has no function call and praser takes care of '''
+gender=5
+height=5.656357
+precision=2
+id=777
+print(f'Hello {height:{precision}.0f}')
+print(f"My height is {height:{10}.{0}f}") #with 10 precision it creating a distance from before character
+print(f"Hello {name=}")
+print(f"hello {id!s}")
+#print(f"hello {}")
