eeb0c278fd3c00039cf7e3f17c1377911ff71c03
chandu reddim <chandu.reddim@gmail.com>
2026-05-10 16:33:23 +0530

    added timeit module

--- strmethods.py
+++ strmethods.py
@@ -12,3 +12,17 @@ print(ord('a'))
 print(ord('A'))
 print(chr(65))
 
+import timeit
+setup_code='filename="script.sh"'
+#filename='script.py'
+
+test_or='filename.endswith(".py") or filename.endswith(".txt") or filename.endswith(".sh")'
+
+test_tuple='filename.endswith((".py",".txt",".sh"))'
+
+time_or=timeit.timeit(test_or,setup=setup_code,number=5000000)
+time_tuple=timeit.timeit(test_tuple,setup=setup_code,number=5000000)
+
+print(f"time for or is {time_or:.{2}f} and for the tuple is {time_tuple:.{2}f}")
+
+#with tuple it works faster but keep in mind that due to short circuit evaluation or can be quicker if first or happens to be true it did not check for next.
