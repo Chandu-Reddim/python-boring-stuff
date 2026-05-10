a6aef1aa2c9b58cc6d9588b3206628879f0a89d3
chandu reddim <chandu.reddim@gmail.com>
2026-05-10 16:04:26 +0530

    second file about some more string methods

--- FstringExp.py
+++ FstringExp.py
@@ -8,11 +8,11 @@ dis.dis(formatVsFstring)
 
 '''F string are better than format string as it has no function call and praser takes care of '''
 gender=5
-height=5.656357
+height=5666.656357
 precision=2
 id=777
 print(f'Hello {height:{precision}.0f}')
-print(f"My height is {height:{10}.{0}f}") #with 10 precision it creating a distance from before character
+print(f"My height is {height:{2}.{3}f}") #with 10 precision it creating a distance from before character
 print(f"Hello {name=}")
 print(f"hello {id!s}")
 #print(f"hello {}")
---
+++ strmethods.py
@@ -0,0 +1,4 @@
+print('-'.join(['a','b','c','c']))
+# join adds the - with every parameter provided.
+# join works quicker than the for loop and concetenation with + as it leads to creating many intermediate strings along the way
+
