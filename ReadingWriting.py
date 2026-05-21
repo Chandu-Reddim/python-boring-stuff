054e36d031a90d6b26f5ab120e6477f93da81814
chandu reddim <chandu.reddim@gmail.com>
2026-05-22 01:34:23 +0530

    a little more about parents, parts and stat() method

--- ReadingWriting.py
+++ ReadingWriting.py
@@ -22,3 +22,19 @@ print((Path.cwd() / 'test2').is_absolute())
 print(Path('test2').absolute())
 p = Path.cwd()
 print(f'Parent is {p.parent} and drive is {p.drive} , anchor is {p.anchor}, name is {p.name}, stem/base is {p.stem} and suffix is {p.suffix}')
+print(p.parts)
+print(p.parents[0])
+# Parents method does not return a list object to apply slicing so only [number] works and parents method is to get the ancestral path from existing
+try:
+    print(p.parents[1])
+except Exception:
+    print("error raised")
+
+p = Path.cwd() / 'test2'
+print(f' 0 is {p.parents[0]} and 1 is {p.parents[1]}')
+
+# for D:/a/b 0 - D:/a for 1 - D:/ ( 2  steps lower)
+
+# to get stats of a object
+print(p.stat())
+print(p.stat().st_file_attributes)
